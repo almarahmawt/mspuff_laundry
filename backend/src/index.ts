0caa2f250975e058ee458b14a0a659144bb2ea25
Mzarifin59 <arifinrifky326@gmail.com>
2025-06-16 10:47:23 +0700

    refactor: Create API Service Middleware to replace lifecycles hooks and replace production frontend ports

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-// import type { Core } from '@strapi/strapi';
+import type { Core } from "@strapi/strapi";
 
 export default {
   /**
@@ -7,7 +7,58 @@ export default {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/* { strapi }: { strapi: Core.Strapi } */) {},
+  register({ strapi }: { strapi: Core.Strapi }) {
+    strapi.documents.use(async (context, next) => {
+      // Menghitung total_harga sebelum update pada transaksi
+      if (
+        context.uid == "api::transaksi.transaksi" &&
+        context.action == "update"
+      ) {
+        const recordId = context.params.documentId;
+        let totalHarga = 0;
+
+        // Ambil data transaksi beserta detailnya
+        const transaksi = await strapi
+          .documents("api::transaksi.transaksi")
+          .findOne({
+            documentId: recordId,
+            populate: ["detail_transaksis"],
+          });
+
+        // Pastikan detail_transaksis ada dan merupakan array
+        if (transaksi && Array.isArray(transaksi.detail_transaksis)) {
+          transaksi.detail_transaksis.forEach((item) => {
+            totalHarga += item.subtotal;
+          });
+        }
+
+        context.params.data.total_harga = totalHarga;
+      }
+
+      // Menghitung subtotal pada detail transaksi
+      if (
+        context.uid == "api::detail-transaksi.detail-transaksi" &&
+        context.action == "update"
+      ) {
+        const recordId = context.params.documentId;
+
+        const layanans = await strapi
+          .documents("api::detail-transaksi.detail-transaksi")
+          .findOne({
+            documentId: recordId,
+            populate: ["layanan"],
+          });
+
+        if (context.params.data.layanan && context.params.data.jumlah) {
+          const hargaLayanan = Number(layanans.layanan?.harga ?? 0);
+          const jumlah = Number(context.params.data.jumlah ?? 0);
+          context.params.data.subtotal = hargaLayanan * jumlah;
+        }
+      }
+
+      return next();
+    });
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
